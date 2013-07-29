aa3d20bb657c98788cc4ff20035bf718b758d303
Juan Carlos Vega Abarca <juanvegab23@gmail.com>
2013-07-29 11:03:52 -0600

    new projects

---
+++ setratadevinos/template/footer.php
@@ -0,0 +1,14 @@
+  <div id="DIVfooter">
+   <div>
+    <p>Tel&eacute;fono: 
+    	<a href="tel:(506) 83197601" title="8319 7601">(506) 8319-7601</a> &nbsp;&nbsp;&nbsp; 
+    	<a href="tel:(506) 83283105" title="8328 3105">(506) 8328-3105</a> &nbsp;&nbsp;&nbsp;
+    	<a href="tel:(34) 686986792" title="686 986 792">(34) 686 986 792</a>
+    </p>
+    <p>E-mail: <a href="mailto:info@setratadevinos.com" title="Correo electr&oacute;nico - Se trata de vinos">info@setratadevinos.com</a></p>
+   </div>
+   <div>
+    <p>Se Trata de Vinos Derechos Reservados</p>
+    <p>Desarrollado por: <a href="mailto:juancarlos@vegabarca.com" title="www.vegabarca.com">www.vegabarca.com</a></p>
+   </div>
+  </div>
